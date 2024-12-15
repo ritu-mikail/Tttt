2ee8e6d75819af2e21586c7767bcfe04416c0b1f
Tarek-bot-009 <tareksarkar620@gmail.com>
2024-12-15 18:52:35 +0600

    Update joinNoti.js

--- Priyansh/events/joinNoti.js
+++ Priyansh/events/joinNoti.js
@@ -1,117 +1,64 @@
 module.exports.config = {
-    name: "joinNoti",
-    eventType: ["log:subscribe"],
-    version: "1.0.1",
-    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-    description: "Notification of bots or people entering groups with random gif/photo/video",
-    dependencies: {
-        "fs-extra": "",
-        "path": "",
-        "pidusage": ""
-    }
+  name: "joinNoti",
+  eventType: ["log:subscribe"],
+  version: "1.0.1",
+  credits: "nazrul",
+  description: "Notify bots or people entering the group",
+  dependencies: {
+    "fs-extra": ""
+  }
 };
- 
-module.exports.onLoad = function () {
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { join } = global.nodemodule["path"];
- 
-    const path = join(__dirname, "cache", "joinvideo");
-    if (existsSync(path)) mkdirSync(path, { recursive: true }); 
- 
-    const path2 = join(__dirname, "cache", "joinvideo", "randomgif");
-    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
- 
-    return;
-}
- 
- 
 module.exports.run = async function({ api, event }) {
-    const { join } = global.nodemodule["path"];
-    const { threadID } = event;
-    if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-        api.changeNickname(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? " " : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
-        const fs = require("fs");
-        return api.sendMessage("", event.threadID, () => api.sendMessage({body: `🍒💙•••Ɓ❍ʈ Ƈøɳɳɛƈʈɛɗ•••💞🌿
-        
-🕊️🌸...Ɦɛɭɭ❍ Ɠɣus Ɱɣ Ɲɑɱɛ Is 🍒💙•••✦𝘽𝙤𝙩✦•••💞🌿
-
-
-
-
- ✨💞Ɱɣ Ꭾɽɛfɪᵡ ɪs / 
-
-
-\n\nƬɣƥɛ${global.config.PREFIX}ꞪɛɭᎮ Ƭ❍ søø Ɱɣ Ƈøɱɱɑɳɗ ɭɪsʈ...🤍💫\n
-\nƐxɑɱƥɭɛ :\n
-
-${global.config.PREFIX}Sɧɑɣɽɪ..💜(Ƭɛxʈ)\n${global.config.PREFIX} (Ƥɧøʈø)🌬️🌳🌊
-
-🦋🌸Ƭɣƥɛ${global.config.PREFIX}Ɦɛɭƥ2 (Ɑɭɭ Ƈøɱɱɑɳɗʂ)...☃️💌
-
-${global.config.PREFIX} ɪɳfø (ɑɗɱɪɳ Iɳføɽɱɑʈɪøɳ)👀✍️
-...🍫🥀Ɱɣ ❍wɳɛɽ ɪs Ɱɽ ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙...🕊️☃️
-
-${global.config.PREFIX}🌺🍃Ƈɑɭɭɑɗ føɽ Ɑɳɣ ɪʂʂuɛ 
-<<<<<------------------------------>>>>>
-A̸N̸D̸ F̸O̸R̸ A̸N̸Y̸ R̸E̸P̸O̸R̸T̸ O̸R̸ C̸O̸N̸T̸A̸C̸T̸ B̸O̸T̸ D̸E̸V̸A̸L̸O̸P̸A̸R̸....💙🍫
-
-💝🥀𝐎𝐖𝐍𝐄𝐑:- ☞T̷a̷r̷e̷k̷ ̷S̷a̷r̷k̷a̷r̷☜ 💫\n🖤𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 𓊈𒆜🆃🅰🆁🅴🅺𒆜𓊉🖤\n😳𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝🤓:- ☞ https://www.facebook.com/tarek.20.king\n
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @tareksarker2
-
-
-✮☸✮
-✮┼💞┼✮
-☸🕊️━━•🌸•━━🕊️☸
-✮☸✮
-✮┼🍫┼✮
-☸🎀━━•🧸•━━🎀☸
-✮┼🦢┼✮
-✮☸✮
-☸🌈━━•🤍•━━🌈☸
-✮☸✮
-✮┼❄️┼✮
-
-┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓🌸✦✧✧✧✧✰🍒𓊈𒆜🆃🅰🆁🅴🅺𒆜𓊉🌿✰✧✧✧✧✦🌸  ┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛
-`, attachment: fs.createReadStream(__dirname + "/cache/botjoin.mp4")} ,threadID));
-    }
-    else {
-        try {
-            const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
-            let { threadName, participantIDs } = await api.getThreadInfo(threadID);
  
-            const threadData = global.data.threadData.get(parseInt(threadID)) || {};
-            const path = join(__dirname, "cache", "joinvideo");
-            const pathGif = join(path, `${threadID}.video`);
+  const request = require("request");
+  const { threadID } = event;
+  if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+    api.changeNickname(`❤️💛𝙏𝙖𝙧𝙚𝙠 𝘽𝙤𝙩❤️💛`, threadID, api.getCurrentUserID());
+    return api.sendMessage(`🥀🥀আসসালামু আলাইকুম 🥀🥀\nআসা করি সাবাই আল্লাহ রহমতে ভালো আছেন\nযে কোন কমান্ড দেখতে ${global.config.PREFIX}help ব্যবহার করুন\n╔══════☆♡☆══════╗\n╰┈➤উদাহারণ\n\n╰┈➤${global.config.PREFIX}inf\n╰┈➤${global.config.PREFIX}admin \n╰┈➤${global.config.PREFIX}owner  \n╰┈➤${global.config.PREFIX}help \n┗━━━ ✦❘༻༺❘✦ ━━━━┛\n
+★যেকোনো অভিযোগ অথবা হেল্প এর জন্য আমার\nবস 𝙏𝙖𝙧𝙚𝙠 কে নক করতে পারেন\n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/tarek.20.king\n𝐌𝐄𝐒𝐒𝐄𝐍𝐆𝐀𝐑𝐄: https://m.me/tarek.20.king`, threadID);
+  }
+  else {
+    try {
+    const request = require("request");
+      const fs = global.nodemodule["fs-extra"];
+      let { threadName, participantIDs } = await api.getThreadInfo(threadID);
  
-            var mentions = [], nameArray = [], memLength = [], i = 0;
-            
-            for (id in event.logMessageData.addedParticipants) {
-                const userName = event.logMessageData.addedParticipants[id].fullName;
-                nameArray.push(userName);
-                mentions.push({ tag: userName, id });
-                memLength.push(participantIDs.length - i++);
+      const threadData = global.data.threadData.get(parseInt(threadID)) || {};
+ 
+      var mentions = [], nameArray = [], memLength = [], i = 0;
+ 
+    let addedParticipants1 = event.logMessageData.addedParticipants;
+        for (let newParticipant of addedParticipants1) {
+   let userID = newParticipant.userFbId
+api.getUserInfo(parseInt(userID), (err, data) => {
+      if(err){ return console.log(err)}
+     var obj = Object.keys(data);
+    var userName = data[obj].name.replace("@", "");     	if (userID !== api.getCurrentUserID()) {  
+ 
+        nameArray.push(userName);
+        mentions.push({ tag: userName, id: userID, fromIndex: 0 });
+ 
+        memLength.push(participantIDs.length - i++);
+memLength.sort((a, b) => a - b);
+ 
+      (typeof threadData.customJoin == "undefined") ? msg = "╔════•|      ✿      |•════╗\n   আ্ঁস্ঁসা্ঁলা্ঁমু্ঁ💚আ্ঁলা্ঁই্ঁকু্ঁম্ঁ\n╚════•|      ✿      |•════╝\n\n    {name} \nআ্ঁপ্ঁনা্ঁকে্ঁ আ্ঁমা্ঁদে্ঁর্ঁ\n{threadName}\nএ্ঁর্ঁ প্ঁক্ষ্ঁ থে্ঁকে্ঁ ভা্ঁলো্ঁবা্ঁসা্ঁ অ্ঁভি্ঁরা্ঁম্ঁ\n\nআঁপঁনিঁ এঁইঁ গ্রুঁপেঁর {soThanhVien} নঁং মে্ঁম্বা্ঁরঁ\n\n𝙏𝙖𝙧𝙚𝙠 ব্ঁট্ঁ পঁক্ষঁ থেঁকেঁ আঁপঁনাঁকেঁ স্বাঁগঁতঁমঁ♥\n🥰🥀ᏔᎬᏞᏟϴᎷᎬ 🥀🥰 \n     ┌────♣─────┐\n     😘♦ -তারেক-  ♦😘\n     └────♣─────┘\n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝙏𝙖𝙧𝙚𝙠 -----❖----" : msg = threadData.customJoin;
+      msg = msg
+      .replace(/\{name}/g, nameArray.join(', '))
+      .replace(/\{type}/g, (memLength.length > 1) ?  'you' : 'Friend')
+      .replace(/\{soThanhVien}/g, memLength.join(', '))
+      .replace(/\{threadName}/g, threadName);			
+ 
+      var link = [
+"https://i.imgur.com/7yZ58P8.jpeg",
+      ];
+        var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/wellcome.jpg"), mentions }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/wellcome.jpg"));
+    return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/wellcome.jpg")).on("close", () => callback());       
             }
-            memLength.sort((a, b) => a - b);
-            
-            (typeof threadData.customJoin == "undefined") ? msg = "Hello Mr/Miss {name},\n─────────────────\n You're The {soThanhVien}Member ─────────────────\nOf {threadName} Group\n─────────────────\nPlease Enjoy Your Stay\n─────────────────\nAnd Make Lots Of Friends =)\n──────-°°__𝗧𝗿𝘂𝘀𝘁 𝗺e 🔐 °__!!>☁️✨❤️ My Owner  ✦͙͙͙͙❥⃝∗⁎.ʚ 𝙏𝙖𝙧𝙚𝙠 𝙎𝙖𝙧𝙠𝙖𝙧 ɞ.⁎∗❥⃝**͙✦͙͙͙ ❤️ Love you 😘 ummmma ❤️😍" : msg = threadData.customJoin;
-            msg = msg
-            .replace(/\{name}/g, nameArray.join(', '))
-            .replace(/\{type}/g, (memLength.length > 1) ?  'Friends' : 'Friend')
-            .replace(/\{soThanhVien}/g, memLength.join(', '))
-            .replace(/\{threadName}/g, threadName);
- 
-            if (existsSync(path)) mkdirSync(path, { recursive: true });
- 
-            const randomPath = readdirSync(join(__dirname, "cache", "joinGif", "randomgif"));
- 
-            if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathvideo), mentions }
-            else if (randomPath.length != 0) {
-                const pathRandom = join(__dirname, "cache", "joinGif", "randomgif", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
-                formPush = { body: msg, attachment: createReadStream(pathRandom), mentions }
-            }
-            else formPush = { body: msg, mentions }
- 
-            return api.sendMessage(formPush, threadID);
-        } catch (e) { return console.log(e) };
+})
+        }
+    }catch (err) {
+            return console.log("ERROR: "+err);
     }
-              }
+  }
+     }
+ 
