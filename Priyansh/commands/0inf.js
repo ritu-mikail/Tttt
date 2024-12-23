483cf4674107823e3788040373fa4d8fe5a522c2
Tarek-bot-009 <tareksarkar620@gmail.com>
2024-12-23 12:38:17 +0600

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -24,33 +24,33 @@ const time = process.uptime(),
 		seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-var link = ["https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg", 
+var link = ["https://i.imgur.com/2XeH10l.jpeg", 
             
-            "https://i.postimg.cc/28zrCYPW/received-2253672584826375.jpg", 
+            "https://i.imgur.com/2XeH10l.jpeg", 
             
-            "https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg",
+            "https://i.imgur.com/2XeH10l.jpeg",
 
-"https://i.postimg.cc/3JpSf0LC/received-1709835639483729.jpg",
+"https://i.imgur.com/2XeH10l.jpeg",
             
-            "https://i.postimg.cc/28zrCYPW/received-2253672584826375.jpg"];
+            "https://i.imgur.com/2XeH10l.jpeg"];
   
 var callback = () => api.sendMessage({body:` 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡
 ======================
 𝗕𝗢𝗧 𝗡𝗔𝗠𝗘 : ${global.config.BOTNAME}
-𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 : 𝗡𝗔𝗭𝗥𝗨𝗟
-𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝗡𝗔𝗢𝗚𝗔𝗢𝗡
+𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 : 𝐓𝐚𝐫𝐞𝐤 𝐒𝐚𝐫𝐤𝐚𝐫
+𝗔𝗗𝗗𝗥𝗘𝗦𝗦: 𝐌𝐲𝐦𝐞𝐧𝐬𝐢𝐧𝐠𝐡
 ======𝗖𝗢𝗡𝗧𝗔𝗖𝗧======
 ======================
-𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/ji.la.pi.6
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/tarek.20.king
 𝗠𝗔𝗦𝗦𝗘𝗡𝗚𝗘𝗥 𝗖𝗛𝗔𝗧 𝗟𝗜𝗡𝗞:
-https://m.me/ji.la.pi.6
+https://m.me/tarek.20.king
 𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫: ${global.config.PREFIX}
-𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: 𝗡𝗔𝗭𝗥𝗨𝗟
+𝗕𝗢𝗧 𝗪𝗢𝗡𝗘𝗥: 𝐓𝐚𝐫𝐞𝐤 
 𝗢𝗧𝗛𝗘𝗥 𝗡𝗘𝗢𝗥𝗠𝗔𝗧𝗧𝗢𝗡 ============================================
 𝗧𝗢𝗗𝗬 𝗜𝗦 𝗧𝗜𝗠𝗘 : ${juswa} 
 𝗕𝗢𝗧 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚 ${hours}:${minutes}:${seconds}.
 𝗧𝗛𝗔𝗡𝗞𝗦 𝗙𝗢𝗥 𝗨𝗦𝗜𝗡𝗚 ${global.config.BOTNAME} \n======================
 ✢━━━━━ N/S ━━━━━✢
-----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖---- `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+----❖----- 𝐓𝐚𝐫𝐞𝐤 -----❖---- `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
    };
