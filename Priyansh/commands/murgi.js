943f557f121f43f28c3ee05d8ab63480fae56ba9
Tarek-bot-009 <tareksarkar620@gmail.com>
2024-12-31 21:52:39 +0600

    Update murgi.js

--- Priyansh/commands/murgi.js
+++ Priyansh/commands/murgi.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "murgi",
     version: "1.0.0",
-    hasPermssion: 2,
+    hasPermssion: 0,
     credits: "NAZRUL",
     description: "5 বারের জন্য ক্রমাগত বন্ধুর ট্যাগ ট্যাগ করুন\nসেই ব্যক্তিকে আত্মা কলিং বলা যেতে পারে",
     commandCategory: "nsfw",
