2b251894d015986a6d202d4250bfd5ed321e7c0c
Tarek-bot-009 <tareksarkar620@gmail.com>
2025-01-01 18:16:12 +0600

    Update rag.js

--- Priyansh/commands/rag.js
+++ Priyansh/commands/rag.js
@@ -8,7 +8,7 @@ const link = [
 ];
  
 module.exports.config = {
-  name: "🤬😡😾",
+  name: "🤬",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "nazrul",
@@ -26,7 +26,7 @@ module.exports.config = {
 module.exports.handleEvent = async ({ api, event, Threads }) => {
   const content = event.body ? event.body : '';
     const body = content.toLowerCase();
-  if (body.startsWith("🤬",😡","😾")) {
+  if (body.startsWith("🤬")) {
     const rahad = [
       "কিরে আবাল তুই রাগ কারে দেখাছ😾"
     
