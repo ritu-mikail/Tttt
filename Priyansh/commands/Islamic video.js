34f8145c4e80548e969ef21fb810e6e39bbf3165
Tarek-bot-009 <tareksarkar620@gmail.com>
2025-01-15 18:21:05 +0600

    Create Islamic video.js

---
+++ Priyansh/commands/Islamic video.js
@@ -0,0 +1,48 @@
+/** THIS FULL BOT COMMAND FILE AND ALL API CREATE BY BADOL CHAT DONT CHINGE THE MY ANY CODE 🤙🖤📿  **/
+module.exports.config = {
+name: "islam",
+  version: "1.0.0",
+  haspermssion: 0,
+  credits: "000",
+  prefix:true,
+  description: "Islamic video",
+  useprefix:true,
+  commandCategory: "Khan Rahul RK",
+  usages: "Islamic video",
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+   var hi = ["✰❥⏤͟͟͞͞◆🤍🪽\n\n\●❥𝄞⋆⃝🌺✨━𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐕𝐢d𝐞𝐨━●❥Tarek Sarkar𝄞⋆⃝🌺\n\n\n•⎯͢⎯⃝🩷🫶🏻"];
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link =[
+
+"https://i.imgur.com/ydiLocz.mp4",
+"https://i.imgur.com/lCtItXr.mp4",
+"https://i.imgur.com/PbBa6W9.mp4",
+"https://i.imgur.com/mZvbNRZ.mp4",
+"https://i.imgur.com/fljfIWV.mp4",
+"https://i.imgur.com/ML2we6E.mp4",
+"https://i.imgur.com/ML2we6E.mp4",
+"https://i.imgur.com/oxYEI8J.mp4",
+"https://i.imgur.com/sFEAbZY.mp4",
+"https://i.imgur.com/6uPsQ4C.mp4",
+"https://i.imgur.com/MCA4duI.mp4",
+"https://i.imgur.com/yhJZHp2.mp4",
+"https://i.imgur.com/Iix6A7f.mp4",
+"https://i.imgur.com/S5hluTk.mp4",
+"https://i.imgur.com/pWbvRzB.mp4",
+"https://i.imgur.com/q771gcD.mp4",
+"https://i.imgur.com/nwsKKir.mp4",
+];
+     var callback = () => api.sendMessage({body:` ${know} `,attachment: fs.createReadStream(__dirname + "/cache/15.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/15.mp4"));    
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/15.mp4")).on("close",() => callback());
+   };
